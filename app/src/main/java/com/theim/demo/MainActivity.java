229ad4881517af3e1fd1eadd871c245d53c12faf
anuchit <anuchit.im@gmail.com>
2019-09-23 15:43:54 +0700

    Acitvity 1

--- app/src/main/java/com/theim/demo/MainActivity.java
+++ app/src/main/java/com/theim/demo/MainActivity.java
@@ -2,13 +2,63 @@ package com.theim.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("DEMO_DEBUG","onCreate");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("DEMO_DEBUG","onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("DEMO_DEBUG","onDestroy");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("DEMO_DEBUG","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("DEMO_DEBUG","onStop");
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("แจ้งเตือน")
+                .setMessage("ยืนยันปิด?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        onBackPressed2();
+                    }})
+                .setNegativeButton(android.R.string.no, null).show();
+
+        Log.i("DEMO_DEBUG","onBackPressed");
+    }
+
+    private void onBackPressed2(){
+        super.onBackPressed();
     }
 }
